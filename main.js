11f3a0ea97f9306772e175718f613b893d0dc317
undef1ned7 <myizambekov.baiel9@gmail.com>
2025-09-10 19:34:51 +0600

    commit

--- main.js
+++ main.js
@@ -16,3 +16,19 @@ btn.addEventListener("click", () => {
 
 console.log(1);
 console.log(2);
+
+// let btn = document.querySelector(".mobile-btn");
+
+// const toggleBurger = (menuClass, showMenuClass) => {
+//   let menu = document.querySelector(`.${menuClass}`);
+
+//   if (menu.className.includes(showMenuClass)) {
+//     menu.className = menuClass;
+//   } else {
+//     menu.className += ` ${showMenuClass}`;
+//   }
+// };
+
+// btn.addEventListener("click", () => {
+//   toggleBurger("header-nav", "header-nav-show");
+// });
